57476458fa39bffc9ca990063297f219538e18b8
RebGov <becci.govert@gmail.com>
2018-07-22 21:15:54 -0500

    triangle and raise error learned

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
 class Triangle
-  # write code here
+
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    validate_triangle
+    if a == b && b == c
+      :equilateral
+    elsif a ==b || b==c || a==c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def validate_triangle
+    true_triangle = [(a+b>c), (a+c>b), (b+c>a)]
+    [a,b,c].each do |s|
+      true_triangle << false if s <= 0
+    end
+      raise TriangleError if true_triangle.include?(false)
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
